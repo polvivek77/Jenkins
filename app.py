0838ca4d4a864ebdaac7eb6207d75518a677486f
Vivek Pol <vivekpol77@gmail.com>
2026-06-15 21:05:05 +0530

    made some changes.

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "My name is Vivek Pol. Please do like share and subscribe to my YouTube Channel - BetterCallPol"
+    return "My name is Vivek Pol. If you like my efforts then please do like share and subscribe to my YouTube Channel - BetterCallPol"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
